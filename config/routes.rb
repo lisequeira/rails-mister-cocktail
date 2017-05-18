8205ca70a4a8509b472e9777e635bff4542f3110
Liliana Sequeira <lilianasrsequeira@gmail.com>
2017-05-18 15:23:23 +0100

    Adding ingredients done - views need work

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'ingredients/new'
+
+  get 'ingredients/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails do
     resource :doses, only: [:new, :create]
@@ -8,5 +12,7 @@ Rails.application.routes.draw do
   resources :doses, only: [:destroy]
 
   root :to => "cocktails#index"
+
+  resources :ingredients, only: [:new, :create, :index]
 
 end
