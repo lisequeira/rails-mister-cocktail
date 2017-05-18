655ab10ae2b0922982bbe77965a6ca007d6daf48
Liliana Sequeira <lilianasrsequeira@gmail.com>
2017-05-18 14:34:11 +0100

    Prepare for Heroku

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,45 @@
+class CocktailsController < ApplicationController
+
+   before_action :set_cocktail, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def edit
+  end
+
+  def create
+    @cocktail = Cocktail.new(set_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
+
+  end
+
+  def update
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def show
+  end
+
+  def destroy
+    @cocktail.destroy
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def set_params
+    params.require(:cocktail).permit(:name)
+  end
+end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,9 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates_presence_of :cocktail, :ingredient
+  validates :description, presence: true
+
+  validates_uniqueness_of :ingredient, scope: [:cocktail]
 end
